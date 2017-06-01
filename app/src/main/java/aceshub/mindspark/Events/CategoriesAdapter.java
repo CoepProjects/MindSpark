b408126b763223b3fab7f452cf8fd541c0623745
gEt-rIgHt-jR <abhidnya.pro@gmail.com>
2017-06-01 16:45:22 +0530

    Card background color issue (issue #1) fixed.

--- app/src/main/java/aceshub/mindspark/Events/CategoriesAdapter.java
+++ app/src/main/java/aceshub/mindspark/Events/CategoriesAdapter.java
@@ -1,6 +1,8 @@
 package aceshub.mindspark.Events;
 
 import android.content.Context;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -23,25 +25,30 @@ public class CategoriesAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
     private List<CategoriesSingleItem> categoriesSingleItem;
     private Context context;
 
-    public CategoriesAdapter(Context context, List<CategoriesSingleItem> categoriesSingleItems){
-        this.categoriesSingleItem=categoriesSingleItems;
-        this.context=context;
+    public CategoriesAdapter(Context context, List<CategoriesSingleItem> categoriesSingleItems) {
+        this.categoriesSingleItem = categoriesSingleItems;
+        this.context = context;
     }
+
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.category_single_item,parent,false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.category_single_item, parent, false);
         return new CategoryViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        final CategoryViewHolder categoryViewHolder=(CategoryViewHolder)holder;
+        final CategoryViewHolder categoryViewHolder = (CategoryViewHolder) holder;
         categoryViewHolder.tvCategory.setText(categoriesSingleItem.get(position).getCategoryName());
         categoryViewHolder.ivCategory.setImageResource(categoriesSingleItem.get(position).getImage());
 
-        List<EventsSingleItem> eventsSingleItems=categoriesSingleItem.get(position).getEventsSingleItemList();
+        int colorId = categoriesSingleItem.get(position).getBackgroundColor();
+        int color = ContextCompat.getColor(context, colorId);
+        categoryViewHolder.cvCategories.setCardBackgroundColor(color);
 
-        EventsAdapter eventsAdapter=new EventsAdapter(eventsSingleItems);
+        List<EventsSingleItem> eventsSingleItems = categoriesSingleItem.get(position).getEventsSingleItemList();
+
+        EventsAdapter eventsAdapter = new EventsAdapter(eventsSingleItems);
 
         categoryViewHolder.rvEvents.setItemAnimator(new DefaultItemAnimator());
         categoryViewHolder.rvEvents.setLayoutManager(new LinearLayoutManager(context));
@@ -50,12 +57,15 @@ public class CategoriesAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
         categoryViewHolder.llCategories.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                categoryViewHolder.rvEvents.setVisibility(View.VISIBLE);
+                if (categoryViewHolder.rvEvents.getVisibility() == View.GONE)
+                    categoryViewHolder.rvEvents.setVisibility(View.VISIBLE);
+                else
+                    categoryViewHolder.rvEvents.setVisibility(View.GONE);
             }
         });
 
         categoryViewHolder.rvEvents.setVisibility(View.GONE);
-        categoryViewHolder.llCategories.setBackgroundColor(categoriesSingleItem.get(position).getBackgroundColor());
+        // categoryViewHolder.llCategories.setBackgroundColor(categoriesSingleItem.get(position).getBackgroundColor());
     }
 
     @Override
@@ -68,12 +78,15 @@ public class CategoriesAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
         TextView tvCategory;
         RecyclerView rvEvents;
         LinearLayout llCategories;
+        CardView cvCategories;
+
         public CategoryViewHolder(View itemView) {
             super(itemView);
-            ivCategory=(ImageView)itemView.findViewById(R.id.ivCategory);
-            tvCategory=(TextView)itemView.findViewById(R.id.tvCategory);
-            rvEvents=(RecyclerView)itemView.findViewById(R.id.rvEvents);
-            llCategories=(LinearLayout) itemView.findViewById(R.id.llCategoryCard);
+            ivCategory = (ImageView) itemView.findViewById(R.id.ivCategory);
+            tvCategory = (TextView) itemView.findViewById(R.id.tvCategory);
+            rvEvents = (RecyclerView) itemView.findViewById(R.id.rvEvents);
+            llCategories = (LinearLayout) itemView.findViewById(R.id.llCategoryCard);
+            cvCategories = (CardView) itemView.findViewById(R.id.cvCategory);
         }
 
     }
