ea5fe982ad21f4854346ea0fbfde574ef29622e2
Ashish Pawar <ashishpawar2015.ap@gmail.com>
2017-06-02 20:30:42 +0530

    Sponsors vertical pager added

--- app/src/main/java/aceshub/mindspark/SponsorsFragment.java
+++ app/src/main/java/aceshub/mindspark/SponsorsFragment.java
@@ -15,6 +15,6 @@ public class SponsorsFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.sponsors,container,false);
+        return inflater.inflate(R.layout.fragment_sponsors,container,false);
     }
 }
