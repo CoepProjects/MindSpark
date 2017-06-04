6b0c80337ba24fb0b3081a200acc6bfb1bf6210d
Ashish Pawar <ashishpawar2015.ap@gmail.com>
2017-06-04 15:05:41 +0530

    Runtime Permissions added

--- app/src/main/java/aceshub/mindspark/Location/LocationFragment.java
+++ app/src/main/java/aceshub/mindspark/Location/LocationFragment.java
@@ -1,15 +1,21 @@
 package aceshub.mindspark.Location;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.PermissionRequest;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -21,11 +27,17 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import aceshub.mindspark.R;
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnNeverAskAgain;
+import permissions.dispatcher.OnPermissionDenied;
+import permissions.dispatcher.RuntimePermissions;
+
 
 /**
  * Created by Ashish Pawar(ashishpawar2015.ap@gmail.com) on 23/5/17.
  */
 
+@RuntimePermissions
 public class LocationFragment extends Fragment implements OnMapReadyCallback {
 
     private GoogleMap googleMap;
@@ -35,12 +47,20 @@ public class LocationFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_location, container, false);
-
         mMapView = (MapView) rootView.findViewById(R.id.mapView);
+
         mMapView.onCreate(savedInstanceState);
 
         mMapView.onResume(); // needed to get the map to display immediately
 
+        showMap();
+        LocationFragmentPermissionsDispatcher.showMapWithCheck(this);
+
+        return rootView;
+    }
+
+    @NeedsPermission({Manifest.permission.ACCESS_COARSE_LOCATION})
+    public void showMap(){
         try {
             MapsInitializer.initialize(getActivity().getApplicationContext());
         } catch (Exception e) {
@@ -52,19 +72,6 @@ public class LocationFragment extends Fragment implements OnMapReadyCallback {
             public void onMapReady(GoogleMap mMap) {
                 googleMap = mMap;
 
-                // For showing a move to my location button
-                if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    return;
-                }
-                googleMap.setMyLocationEnabled(true);
-
                 // For dropping a marker at a point on the Map
                 LatLng eventLoc = new LatLng(18.531240, 73.855246);
                 googleMap.addMarker(new MarkerOptions().position(eventLoc).title("EventName"));
@@ -73,10 +80,15 @@ public class LocationFragment extends Fragment implements OnMapReadyCallback {
                 CameraPosition cameraPosition = new CameraPosition.Builder().target(eventLoc).zoom(18).build();
                 googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 
+
+                // For showing a move to my location button
+                if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                googleMap.setMyLocationEnabled(true);
+
             }
         });
-
-        return rootView;
     }
 
     @Override
@@ -106,4 +118,24 @@ public class LocationFragment extends Fragment implements OnMapReadyCallback {
         super.onLowMemory();
         mMapView.onLowMemory();
     }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        LocationFragmentPermissionsDispatcher.onRequestPermissionsResult(this,requestCode,grantResults);
+    }
+
+
+
+    @OnPermissionDenied(Manifest.permission.ACCESS_COARSE_LOCATION)
+    void showDeniedForLocation() {
+        Toast.makeText(getContext(), "Permission was denied", Toast.LENGTH_SHORT).show();       //if permission is denied
+    }
+
+    @OnNeverAskAgain(Manifest.permission.ACCESS_COARSE_LOCATION)
+    void showNeverAskForLocation() {
+        Toast.makeText(getContext(), "Please give required Permissions from Settings App", Toast.LENGTH_SHORT).show();       //if never ask again pressed
+    }
+
 }
